165cc71a53c9b99cf9d47a8cea528b8b7b541235
Vitaly <vitalykuklin@gmail.com>
2025-01-27 23:14:59 +0700

    iam on logic operation [ref:no-ref]

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 // alert('Hello World');
-let name = prompt("Enter your name", "");
-alert(`Hello ${name}`);
+// let name = prompt("Enter your name", "");
+// alert(`Hello ${name}`);
+
+// const prmt = prompt("give me a number", "");
+// if (prmt > 0) {
+//     alert("positive");
+// } else if (prmt < 0) {
+//     alert("negative");
+// } else {
+//     alert("zero");
+// }
+
+let result;
+
+if (a + b < 4) {
+  result = 'Мало';
+} else {
+  result = 'Много';
+}
+
+result = a + b < 4 ? 'Мало' : 'Много';
+
+console.log(result);
